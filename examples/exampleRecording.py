210a3637b4e2bee9539e36dd32fdd6c56b89519a
izero3127 <izero3127@etners.com>
2021-07-18 22:30:10 +0900

    record

--- examples/exampleRecording.py
+++ examples/exampleRecording.py
@@ -5,43 +5,34 @@ import numpy as np
 from pyKinectAzure import pyKinectAzure, _k4a
 import cv2
 
-# Path to the module
-# TODO: Modify with the path containing the k4a.dll from the Azure Kinect SDK
-modulePath = 'C:\\Program Files\\Azure Kinect SDK v1.4.1\\sdk\\windows-desktop\\amd64\\release\\bin\\k4a.dll' 
-# under x86_64 linux please use r'/usr/lib/x86_64-linux-gnu/libk4a.so'
-# In Jetson please use r'/usr/lib/aarch64-linux-gnu/libk4a.so'
+# SDK의 k4a.dll 파일 있는 Path 찾아서 바꿔줄 것!
+modulePath = 'C:\\Program Files\\Azure Kinect SDK v1.4.1\\sdk\\windows-desktop\\amd64\\release\\bin\\k4a.dll'
 
 if __name__ == "__main__":
 
-	# Initialize the library with the path containing the module
+	# 모듈 패스를 통해 SDK 열어주기
 	pyK4A = pyKinectAzure(modulePath)
-
-	# Open device
+	# device 열기
 	pyK4A.device_open()
-
-	# Modify camera configuration
+	# configuration 설정
 	device_config = pyK4A.config
 	device_config.color_resolution = _k4a.K4A_COLOR_RESOLUTION_1080P
 	device_config.depth_mode = _k4a.K4A_DEPTH_MODE_WFOV_2X2BINNED
 	print(device_config)
 
-	# Start cameras using modified configuration
+	# configuration에 담긴 내용을 바탕으로 카메라 켜기
 	pyK4A.device_start_cameras(device_config)
-
-	# Start recording
+	# recording 하기
 	pyK4A.start_recording("test.mkv")
 
 	k = 0
 	while True:
 		pyK4A.update()
 
-		# Get the depth image from the capture
 		depth_image_handle = pyK4A.capture_get_depth_image()
 
-		# Check the image has been read correctly
 		if depth_image_handle:
 
-			# Read and convert the image data to numpy array:
 			depth_image = pyK4A.image_convert_to_numpy(depth_image_handle)
 			depth_color_image = cv2.convertScaleAbs (depth_image, alpha=0.05)  #alpha is fitted by visual comparison with Azure k4aviewer results  
 			depth_color_image = cv2.applyColorMap(depth_color_image, cv2.COLORMAP_JET)
@@ -49,12 +40,11 @@ if __name__ == "__main__":
 			cv2.imshow('Colorized Depth Image',depth_color_image)
 			k = cv2.waitKey(1)
 
-			# Release the image
 			pyK4A.image_release(depth_image_handle)
 
 		pyK4A.capture_release()
 
-		if k==27:    # Esc key to stop
+		if k==27:    # Esc 눌러서 녹화 종료
 			break
 
 	pyK4A.device_stop_cameras()
